f0edd79fa17d6c533b9937a5f91ff49cb9201e68
edwang09 <edwang09@gmail.com>
2020-05-25 18:03:18 -0400

    responsive design and api reform7

--- pages/privacypolicy.js
+++ pages/privacypolicy.js
@@ -13,7 +13,7 @@ const client = new MongoClient('mongodb+srv://admin:ti21sNLGy1NuJ5s1@cluster0-8f
 
 
 
-export async function getServerSideProps() {
+export async function getStaticProps() {
   // const content = await (await axios.get(`${APIendpoint}/content?page=privacypolicy`)).data.content
   // const fullPath = path.join(process.cwd(), "admin", "privacypolicy")
   // const fileContents = fs.readFileSync(fullPath, "utf8")
