9ba6c41a4b780d97edfa2b57985dc25c291a781a
edwang09 <edwang09@gmail.com>
2020-06-21 12:14:30 -0400

    verification

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.prepare().then(() => {
     ws.send('something');
   });
   
-  server.listen(3000, (err) => {
+  server.listen(process.env.PORT || 3000, (err) => {
     if (err) throw err
     console.log('> Ready on http://localhost:3000')
   })
